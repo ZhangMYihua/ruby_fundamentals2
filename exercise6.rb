6d23bf0d8f533d8b20ec530e5befd4f1d8f33bc9
zhangmyihua <michaelzhang2@msn.com>
2015-01-20 17:33:22 -0500

    A prompt for adding bananas has been added

--- exercise6.rb
+++ exercise6.rb
@@ -11,3 +11,9 @@ grocery_list.push("rice")
 printlist(grocery_list)
 
 puts "Total items on the list is #{grocery_list.count}"
+
+if grocery_list.include?("bananas")
+	puts "you need to pick up bananas"
+else
+	puts "you do not need to pick up bananas today"
+end
