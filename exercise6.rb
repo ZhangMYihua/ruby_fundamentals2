c4cd417bf225344a42d7aba1b8aca75f17d7a7df
zhangmyihua <michaelzhang2@msn.com>
2015-01-20 17:25:06 -0500

    added rice to the list

---
+++ exercise6.rb
@@ -0,0 +1,11 @@
+grocery_list = ["carrots", "apples", "bananas", "toilet paper", "instant coffee"]
+
+def printlist(x)
+	x.each do |y|
+		puts "* #{y}"
+	end
+end
+
+grocery_list.push("rice")
+
+printlist(grocery_list)
