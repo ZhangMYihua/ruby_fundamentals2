802c9e14e6bcf1a231d5b15eada0c4f924f3891b
zhangmyihua <michaelzhang2@msn.com>
2015-01-20 16:52:49 -0500

    Cleaned up the coding and changed to a float for decimal conversion

--- exercise5.rb
+++ exercise5.rb
@@ -3,7 +3,7 @@ puts "Please provide a temperature in Fahrenheit"
 f = gets.chomp.to_i
 
 def convert(x)
-	(celsius = (x - 32) * 5/9)
+	(celsius = (x - 32) * 5.0/9)
 end
 
-puts convert(f)
+puts "#{f} degrees fahrenheit in celsius is #{convert(f)}"
